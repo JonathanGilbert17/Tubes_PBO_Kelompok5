5b70c8d05e2893e206c313bd5130bb1ebee0299d
JonathanGilbert17 <jonathan.tjahja17@gmail.com>
2026-06-14 14:34:22 +0700

    update koneksi database ke Railway

--- src/java/config/DBConnection.java
+++ src/java/config/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/vtube_db?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://thomas.proxy.rlwy.net:53925/railway?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "aODpIsmeXpyDbAroMZfTekcXJgPKYZAs";
 
     public static Connection getConnection() {
         Connection conn = null;
